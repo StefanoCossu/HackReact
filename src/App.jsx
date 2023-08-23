d167d635780902ff1d5feb958bb350b37ad22bd0
StefanoCossu <cossu.stefano1984@gmail.com>
2023-08-23 17:20:42 +0200

    inizio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="mt-12 bg-red-500 mb-12">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, vitae
+        animi ipsam eum, rem laborum nostrum reiciendis inventore excepturi eius
+        nobis odit quibusdam iusto, sunt quam! Libero mollitia deleniti quaerat.
+      </div>
+    </>
+  );
+}
+
+export default App;
