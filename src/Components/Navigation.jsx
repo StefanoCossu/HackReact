caa774159a1845330f7deffbcf6d4f0f127546ff
StefanoCossu <cossu.stefano1984@gmail.com>
2023-08-23 18:28:51 +0200

    darkMode

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import ThemeSwitcher from "./ThemeSwitcher";
 
 export default function Navigation() {
   return (
     <div>
+      <ThemeSwitcher />
       <Link to="/">Home</Link>
     </div>
   );
---
+++ src/Components/ThemeSwitcher.jsx
@@ -0,0 +1,21 @@
+import { ReactComponent as Light } from "../assets/icons/light.svg";
+import { ReactComponent as Dark } from "../assets/icons/dark.svg";
+import { useState } from "react";
+
+export default function ThemeSwitcher() {
+  const [isDark, setIsDark] = useState(
+    document.documentElement.classList.contains("dark")
+  );
+  const changeTheme = () => {
+    setIsDark((isDark) => !isDark);
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    }
+  };
+
+  return <span onClick={changeTheme}>{isDark ? <Light /> : <Dark />}</span>;
+}
