57af73526b6bd377811609fd66bb0751dade89b1
StefanoCossu <cossu.stefano1984@gmail.com>
2023-09-22 19:49:44 +0200

    try

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,5 +1,5 @@
-import { Link, useNavigate } from "react-router-dom";
-import {  useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import {  useEffect, useState } from "react";
 import { ReactComponent as Hamburger } from "../assets/icons/hamburger.svg";
 import { ReactComponent as User } from "../assets/icons/user.svg";
 import ThemeSwitcher from "./switchers/ThemeSwitcher";
@@ -10,12 +10,12 @@ import { useTranslation } from "react-i18next";
 
 export default function Navigation() {
   const {t} = useTranslation()
-
+  const location = useLocation()
   const navigate = useNavigate()
   const [open,setOpen]= useState(false)
   const signOut = useAuthStore((state) => state.setLoggedOut)
   const profile = useAuthStore((state) => state.profile)
-  const [widht, setWidth] = useState("100px")
+  const [width, setWidth] = useState("100px")
 
   const logOut = () => async () => {
     try{
@@ -28,12 +28,25 @@ export default function Navigation() {
       console.log(error);
     }
   };
+
+  useEffect(()=>{
+    if (location.pathname === "/") {
+      setWidth("100px")
+    }else if (location.pathname === "/search") {
+      setWidth("30vw")
+    }else if (location.pathname === "/sign-in") {
+      setWidth("50vw")
+    }else if(location.pathname === "/profile"){
+      setWidth("50vw")
+    }
+    return
+  },[location])
   
   return (
     <nav>
     <div className="fixed z-30 flex h-12 w-screen items-center bg-gradient-to-r from-[#14496c] from-20% via-[#14496cb3] via-90% to-[#14496cb3] px-2 after:absolute after:bottom-[-1px] after:left-[77px] after:h-[1px] after:w-full  after:content-['']">
       <div className={
-        `after:border-r-6 after:animate-flash fixed left-0 top-[19px] z-[-1] h-[44px] w-[${widht}] border-b-2 border-t-0 border-cyan-400 bg-[#14496c] before:absolute before:bottom-[1px] before:right-[-8px] before:z-[0] before:h-[14px] before:w-[18px] before:skew-x-[-50deg] before:bg-[#11496c] before:content-[''] after:absolute after:bottom-[-2px] after:right-[-10px] after:h-[15px] after:w-[6px] after:skew-x-[-50deg] after:bg-white after:content-[''] transition-duration-2000ms` 
+        `after:border-r-6 after:animate-flash fixed left-0 top-[19px] z-[-1] h-[44px] w-[${width}] border-b-2 border-t-0 border-cyan-400 bg-[#14496c] before:absolute before:bottom-[1px] before:right-[-8px] before:z-[0] before:h-[14px] before:w-[18px] before:skew-x-[-50deg] before:bg-[#11496c] before:content-[''] after:absolute after:bottom-[-2px] after:right-[-10px] after:h-[15px] after:w-[6px] after:skew-x-[-50deg] after:bg-white after:content-[''] transition-duration-2000ms` 
         }
         ></div>
         <div className="text-white flex justify-between md:w-1/2 w-2/3 font-title">
