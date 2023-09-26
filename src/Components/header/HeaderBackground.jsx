08b49e74f27236414a6528cd581d7fa299f563d4
StefanoCossu <cossu.stefano1984@gmail.com>
2023-09-27 01:42:28 +0200

    ok

---
+++ src/Components/header/HeaderBackground.jsx
@@ -0,0 +1,84 @@
+import { useState , useEffect } from "react"
+import { useTranslation } from "react-i18next"
+
+// import "./headr.css"
+
+
+// import "./header"
+export default function HeaderBackground(){
+    const {t}= useTranslation()
+    const cols = 77
+    const rows = 20
+    const [grid, setGrid] = useState()
+    const positions = [
+        [0,1],
+        [0,-1],
+        [1,-1],
+        [-1,1],
+        [1,1],
+        [-1,-1],
+        [1,0],
+        [-1,0],
+    ]
+    const randomGrid = () =>{
+        const grid = []
+        for(let i = 0; i < rows; i++){
+            const row =[]
+            for(let j = 0; j < cols; j++){
+                row.push(Math.floor(Math.random()*2))
+            }
+            grid.push(row)
+        }
+        return grid
+    }
+    const runSimulation = ()=>{
+        setGrid((g) => {
+            const next = g.map((row,i)=>{
+                return row.map((cell,j)=>{
+                    let sum = 0
+                    positions.forEach((position)=>{
+                        const x = i + position[0]
+                        const y = j + position[1]
+                        if(x >= 0 && x < rows && y >= 0 && y < cols){
+                            sum += g[x][y]
+                        }
+                    }) 
+                    if(sum < 2 || sum >3){
+                        return 0
+                    }
+                    if (sum === 3) {
+                        return 1
+                    }
+                    return g[i][j]               
+                })
+            })
+            return next
+        })
+    }
+    useEffect(()=>{
+        setGrid(randomGrid())
+    },[])
+    useEffect(()=>{
+        setInterval(()=>{
+            runSimulation(grid)
+        }, 1600)
+        
+    },[])
+
+    return(
+        <div className="relative flex flex-wrap">
+            {grid && grid.map((rows, i)=>
+            rows.map((col,k)=>(
+                <div key={`${i}/${k}/$`} className={`w-[20px] h-[20px] ${grid[i][k] ? "bg-green-200 dark:bg-cyan-600" : ""}  border-[1px] border-grey-500 dark:border-gray-800` }></div>
+            )))}
+            <div className="w-full absolute top-20">
+            <h1 className="text-center bg-gradient-to-r from-[#00BECC] from-98% via-98%  via-[#541c97] to-[#541c97]  bg-clip-text text-4xl font-extrabold text-transparent dark:from-sky-600 dark:to-sky-100 lg:text-10xl  md:text-8xl font-title py-12">
+          {t("home.title")} {import.meta.env.VITE_PROJECT_NAME}
+          </h1>
+          <h2 className="text-center bg-gradient-to-r from-[#00BECC] to-98% via-[#7E2FE0] bg-clip-text text-3xl font-extrabold text-transparent dark:from-sky-600 dark:to-sky-100 lg:text-6xl md:text-4xl font-title pb-12">
+            {t("home.subtitle")}
+          </h2>
+            </div>
+        </div>
+    )
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,8 @@ import { Helmet } from "react-helmet-async";
 import { useTranslation } from "react-i18next";
 import useAuthStore from "../store/authStore";
 import Prefered from "../Components/Prefered";
+import HeaderBackground from "../Components/header/HeaderBackground";
+
 
 
 export default function Home() {
@@ -16,12 +18,13 @@ export default function Home() {
     <div className="min-h-screen">
       <div className="gap-12 px-12 py-12 md:py-24 md:flex">
         <div className="w-full">
-          <h1 className="text-center bg-gradient-to-r from-[#00BECC] to-98% via-[#7E2FE0] bg-clip-text text-4xl font-extrabold text-transparent dark:from-sky-600 dark:to-sky-100 lg:text:6xl md:text:5xl font-title py-12">
+        <HeaderBackground />
+          {/* <h1 className="text-center bg-gradient-to-r from-[#00BECC] to-98% via-[#7E2FE0] bg-clip-text text-4xl font-extrabold text-transparent dark:from-sky-600 dark:to-sky-100 lg:text:6xl md:text:5xl font-title py-12">
           {t("home.title")} {import.meta.env.VITE_PROJECT_NAME}
           </h1>
           <p className="text-center bg-gradient-to-r from-[#00BECC] to-98% via-[#7E2FE0] bg-clip-text text-3xl font-extrabold text-transparent dark:from-sky-600 dark:to-sky-100 lg:text:5xl md:text:4xl font-title pb-12">
             {t("home.subtitle")}
-          </p>
+          </p> */}
         </div>
       </div>
       {profile && 
