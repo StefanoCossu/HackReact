19c143d5c01caf623ff62a61ea3fee2a8169e8fd
StefanoCossu <cossu.stefano1984@gmail.com>
2023-09-29 02:13:57 +0200

    scroll restoration search

--- src/Pages/Root.jsx
+++ src/Pages/Root.jsx
@@ -6,7 +6,13 @@ import Authentication from "../Components/Authentication";
 export default function Root() {
   return (
     <>
-    <ScrollRestoration />
+    <ScrollRestoration getKey={(location, matches) => {
+    const paths = ["/search"];
+    return paths.includes(location.pathname)
+      ? 
+        location.pathname
+      : 
+        location.key;}}/>
     <Authentication>
       <div className="text-slate-800 dark:bg-slate-900 dark:text-slate-200 min-h-screen antialiased">
       <Navigation />
